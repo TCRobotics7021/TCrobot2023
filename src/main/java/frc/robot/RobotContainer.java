ee41982b97847c73517c051899a311141b483cad
MLakowske <44869298+MLakowske@users.noreply.github.com>
2023-01-28 09:55:10 -0600

    First Commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,79 @@
+package frc.robot;
+
+import javax.swing.plaf.TreeUI;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import frc.robot.autos.*;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+    /* Controllers */
+    private final Joystick  RightStick = new Joystick(1);
+    private final Joystick leftStick = new Joystick(0);
+    private final Joystick OpPanel = new Joystick (2);
+
+    /* Drive Controls */
+    //private final int translationAxis = XboxController.Axis.kLeftY.value;
+    //private final int strafeAxis = XboxController.Axis.kLeftX.value;
+    //private final int rotationAxis = XboxController.Axis.kRightX.value;
+
+    /* Subsystems */
+    public final static Swerve s_Swerve = new Swerve();
+    public final static limeLight_subsystem s_Limelight = new limeLight_subsystem();
+
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+        s_Swerve.setDefaultCommand(
+
+            new TeleopSwerve(
+                s_Swerve, 
+                () -> leftStick.getRawAxis(1), //translational x
+                () -> leftStick.getRawAxis(0),  //translational y
+                () -> RightStick.getRawAxis(0), //rotational
+                () -> false  //robot centric boolean
+            )
+        );
+
+        // Configure the button bindings
+        configureButtonBindings();
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        /* Driver Buttons */
+        new JoystickButton(RightStick, 1).onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
+        new JoystickButton(OpPanel, 1).onTrue(new Calibrate().withTimeout(2));
+        new JoystickButton(OpPanel, 2).onTrue(new AutonomousMove(0,2));
+        new JoystickButton(OpPanel, 3).onTrue(new AutonomousMove(2,0));
+        new JoystickButton(OpPanel, 4).onTrue(new AutonomousMove(2, 2))
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return new exampleAuto(s_Swerve);
+    }
+}
---
+++ src/main/java/frc/robot/commands/AutonomousMove.java
@@ -0,0 +1,112 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+public class AutonomousMove extends CommandBase {
+  /** Creates a new AutonomousMove. */
+  double targetX;
+  double targetY;
+
+  double calcStrafe;
+  double calcTranslation;
+  double calcRotation;
+
+  double currentX;
+  double currentY;
+
+  double errorX;
+  double errorY;
+
+  boolean finished;
+
+  public AutonomousMove(double targetX, double targetY) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.targetX = targetX;
+    this.targetY = targetY;
+    finished = false;
+    addRequirements(RobotContainer.s_Swerve);
+
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    RobotContainer.s_Swerve.resettempOdometry(new Pose2d(0, 0, new Rotation2d(0)));
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+
+    currentX = RobotContainer.s_Swerve.gettempPose().getX();
+    currentY = RobotContainer.s_Swerve.gettempPose().getY();
+
+    errorX = Math.abs(targetX - currentX);
+    errorY = Math.abs(targetY - currentY);
+
+    calcTranslation = Constants.Swerve.autonomousMove_P * (targetX - currentX);
+    calcStrafe = Constants.Swerve.autonomousMove_P * (targetY - currentY);
+
+    if (calcTranslation > 0){ // Set the max and min speed on X coords in positive direction
+      calcTranslation = Math.max(.1,calcTranslation);
+      calcTranslation = Math.min(.5, calcTranslation);
+    }
+
+    if (calcTranslation < 0){ // Set the max and min speed on X coords in negative direction
+      calcTranslation = Math.min(-.1,calcTranslation);
+      calcTranslation = Math.max(-.5, calcTranslation);
+    }
+
+    if (calcStrafe > 0){ // Set the max and min speed on Y coords in positive direction
+      calcStrafe = Math.max(.1,calcStrafe);
+      calcStrafe = Math.min(.5, calcStrafe);
+    }
+
+    if (calcStrafe < 0){ // Set the max and min speed on Y coords in negative direction
+      calcStrafe = Math.min(-.1,calcStrafe);
+      calcStrafe = Math.max(-.5, calcStrafe);
+    }
+
+    RobotContainer.s_Swerve.drive(
+      new Translation2d(calcTranslation, calcStrafe).times(Constants.Swerve.maxSpeed), 
+      0 * Constants.Swerve.maxAngularVelocity, 
+      true, //Fieldcentric - !robotCentricSup.getAsBoolean(), 
+      true
+  );
+    if (errorX <= .05 && errorY <= .05){
+      finished = true;
+    }
+    SmartDashboard.putNumber("Calc Translation",calcTranslation);
+    SmartDashboard.putNumber("Calc Strafe",calcStrafe);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.s_Swerve.drive(
+      new Translation2d(0, 0).times(Constants.Swerve.maxSpeed), 
+      0 * Constants.Swerve.maxAngularVelocity, 
+      true, //Fieldcentric - !robotCentricSup.getAsBoolean(), 
+      true
+    );
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+
+    return finished;
+  }
+}
